ca76a85a22a5f9747007fa755ce2d5646654112d
harvs <115873409+harveym4n4lili@users.noreply.github.com>
2025-08-06 17:26:19 +0100

    Basic State Machine for Player Implemented
    - Learned about script-based state machine concept and written required scripts.
    - Added state machine node with child state nodes and attached their relevant scripts.
    - Refactored player script's state changing to include the player state machine.

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -1,36 +1,30 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
 var cardinal_direction : Vector2 = Vector2.DOWN # Facing direction
 var direction : Vector2 = Vector2.ZERO # Intended movement
 var move_speed : float = 100.0
-var state : String = "idle" # Store player action state
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
 
-# Called when node enters tree for the irst time.
+# Called when node enters tree for the first time.
 func _ready() -> void:
+	state_machine.Initialize(self)
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# position update ---
 	# referring to input maps
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	velocity = direction * move_speed
-	
-	# animation update ---
-	# Is player doing something different?
-	# Did player change direction?
-	if SetState() == true or SetDirection() == true:
-		UpdateAnimation()
 	pass
 	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
-# Method to update cardinal direction variable
+## Method to update cardinal direction variable
 func SetDirection() -> bool:
 	if direction == Vector2.ZERO: # No movement input, don't set direction
 		return false
@@ -51,20 +45,12 @@ func SetDirection() -> bool:
 	cardinal_direction = new_dir
 	return true
 	
-# Method for knowing if player is walking or not
-func SetState() -> bool:
-	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state: # If state did not change, no need to update or trigger animations
-		return false
-	state = new_state
-	return true
-	
-# Method for finding the right animation to play
-func UpdateAnimation() -> void:
+## Method for finding the right animation to play
+func UpdateAnimation( state : String ) -> void:
 	animated_sprite_2d.play( state + "_" + AnimDirection())
 	pass
 	
-# Method for finding right direction string using cardinal direction
+## Method for finding right direction string using cardinal direction
 func AnimDirection() -> String:
 	if cardinal_direction == Vector2.DOWN:
 		return "down"
---
+++ scripts/Player/state_idle.gd
@@ -0,0 +1,27 @@
+class_name State_Idle extends State
+
+@onready var walk: State_Walk = $"../Walk"
+
+## What happens when player ENTERS this state?
+func Enter() -> void:
+	player.UpdateAnimation("idle")
+	pass
+
+## What happens when player EXITS this state?
+func Exit() -> void:
+	pass
+
+## What happens during _process update in this state?
+func Process(_delta: float) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
+	return null
+	
+## What happens during _physics_process update in this state?
+func Physics(_delta: float) -> State:
+	return null
+	
+## What happens with input events in this state?
+func HandleInput(_event: InputEvent) -> State:
+	return null
