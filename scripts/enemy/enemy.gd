b1168797891bcec2663331b8d5fd6fea730d429b
harvs <115873409+harveym4n4lili@users.noreply.github.com>
2025-08-17 01:05:27 +0100

    Changed HP, Added Slime HurtBox
    - Included a slime hurtbox and changed HP

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -7,7 +7,7 @@ signal enemy_destroyed(hurt_box:HurtBox)
 
 const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
 
-@export var hp : int = 3
+@export var hp : int = 5
 
 var cardinal_direction : Vector2 = Vector2.DOWN
 var direction : Vector2 = Vector2.ZERO
